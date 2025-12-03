7a8ed4baae4327fb25e5e07c06081ca4c3a101bb
tnshgarg <tanishg566@outlook.com>
2025-12-03 19:11:38 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,7 @@ app.use((req, res, next) => {
 });
 
 // Middleware
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://adaptrix-capstone.vercel.app',
-    'https://adaptrix-capstone.onrender.com'
-  ],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
